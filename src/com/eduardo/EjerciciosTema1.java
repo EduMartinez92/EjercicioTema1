d97537a5e6c69578a46d08945633f91c4bab2fa2
Eduardo Martinez <2748105@alu.murciaeduca.es>
2023-06-13 15:23:34 +0200

    Initial commit

---
+++ src/com/eduardo/EjerciciosTema1.java
@@ -0,0 +1,40 @@
+package com.eduardo;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[] args) {
+
+        // Enteros
+        byte variable1 = 4;
+        short variable2 = 14;
+        int variable3 = 50;
+        long variable4 = 100;
+
+        // Decimales
+        float variable5 = 1.5f;
+        double variable6 = 22.5d;
+
+        // Booleanos
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        // Texto
+        char variable9 = 'a';
+        String variable10 = "Hola";
+
+
+        //Mostramos por pantalla los valores
+
+        System.out.println("Byte " + variable1);
+        System.out.println("Short " + variable2);
+        System.out.println("Int " + variable3);
+        System.out.println("Long " + variable4);
+        System.out.println("Float " + variable5);
+        System.out.println("Double " + variable6);
+        System.out.println("Boolean " + variable7);
+        System.out.println("Boolean " + variable8);
+        System.out.println("Char " + variable9);
+        System.out.println("String " + variable10);
+
+    }
+}
